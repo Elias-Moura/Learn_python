6697045f20a1d49ca934f72c0fe0a5fa498a79b5
Elias-Moura <elias.smourayt@gmail.com>
2022-11-17 14:08:56 -0300

    Estudando list comprehension

---
+++ CursoOtavio/Secao3_Py_Intermediario/list_comprehension/list_comprehension.py
@@ -0,0 +1,14 @@
+
+lista = []
+
+# list comprehension
+
+lista1 = [valor for valor in range(10)]
+
+# filter -> passar o if depois do for
+
+lista = [valor for valor in range(10) if valor > 5]
+
+print(lista1)
+
+print(lista)
